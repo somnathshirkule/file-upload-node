18c4d34f56596bf5482317b8281c745205a6a8c7
Somnath Shirkule <shirkule.somnath@gmail.com>
2022-11-24 23:07:11 +0530

    updated response msg

--- src/controllers/file-upload-controller.js
+++ src/controllers/file-upload-controller.js
@@ -3,8 +3,8 @@ import { uploadFile } from "../services/file-upload-service.js";
 export const addFileController = (router) => {
     router.post('/api/upload', (req, res) => {
         try {
-            let uploadResponse = uploadFile(req.files.file);
-            res.send({message: uploadResponse})
+            uploadFile(req.files.file);
+            res.send({message: 'uploaded successfully.'})
         } catch (error) {
             res.status(500).send(error);
         }
